6fce6fcb4c6902a01561dc805e5d52cdbd3556df
Wilkusz <121688913+Wilkusz@users.noreply.github.com>
2023-05-12 14:59:13 +0200

    Add files via upload

---
+++ src/components/pagination.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import _ from 'lodash';
+
+const Pagination = ( props ) => {
+
+   const { itemsCount, pageSize, currentPage, onPageChange } = props; // pobranie props
+
+   const pagesCount = Math.ceil(itemsCount / pageSize); //obliczenie ilości
+
+   if (pagesCount === 1) {
+       return null;
+   }
+   const pages =_.range(1, pagesCount + 1);
+
+   return (
+       <nav>
+           <ul className="pagination pagination-sm">
+               {pages.map((item, key) => (
+                   <li key={key} className={ item === currentPage ? 'page-item active' : 'page-item' }>
+                       <a style={{cursor: 'pointer'}} className="page-link" onClick={() => onPageChange(item)}>{item}</a>
+                   </li>
+                   )
+               )}
+           </ul>
+       </nav>
+   )
+};
+
+
+
+export default Pagination;
+
